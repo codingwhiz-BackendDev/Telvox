524e1ecf4ac049c36326fce7898f9b627cf92dad
Emmanuel Oluyemi <e8060097@gmail.com>
2026-06-24 21:13:15 +0100

    remove healthcheck completely

--- passenger_wsgi.py
+++
@@ -1,7 +0,0 @@
-import sys, os
-
-sys.path.insert(0, '/home/diggy/aidigpay.com')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'Telvox.settings'
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
